bea5ff0f1145e7c36884a5273c95967308cedbe3
未央天 <asueeer@163.com>
2023-06-29 21:26:32 +0800

    change bird perf

--- perf_bird.py
+++ perf_bird.py
@@ -51,9 +51,9 @@ if __name__ == "__main__":
     net['r3'].cmd('ethtool -K r3-eth0 tx off')
     net['r3'].cmd('ethtool -K r3-eth1 tx off')
 
-    net['r1'].cmd('bird -c bird-r1-v2.conf -d -s bird-r1.ctl')
-    net['r2'].cmd('bird -c bird-r2-v2.conf -d -s bird-r2.ctl')
-    net['r3'].cmd('bird -c bird-r3-v2.conf -d -s bird-r3.ctl')
+    net['r1'].cmd('bird -c ./bird-r1.conf -d -s bird-r1.ctl')
+    net['r2'].cmd('bird -c ./bird-r2.conf -d -s bird-r2.ctl')
+    net['r3'].cmd('bird -c ./bird-r3.conf -d -s bird-r3.ctl')
     try:
         net.start()
         IPCLI(net)
