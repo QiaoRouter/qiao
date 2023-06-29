1a46ad3adfc3fc521ea97f811571c246ed3d1350
未央天 <asueeer@163.com>
2023-06-29 22:19:14 +0800

    add ping.py for test perf

--- perf_qiao.py
+++ perf_qiao.py
@@ -40,8 +40,10 @@ if __name__ == "__main__":
         net[f'h2_{i}'].cmd('ip -6 route add default via 2001:3:{}::2'.format(i+1))
         net[f'h2_{i}'].cmd('ethtool -K h2_{}-eth0 tx off'.format(i+1))
 
-        net[f'h1_{i}'].cmd('ping h2_{}'.format(i+1))
-        net[f'h2_{i}'].cmd('ping h1_{}'.format(i+1))
+        net[f'h1_{i}'].cmd('nohup python3 ping.py h2_{} &'.format(i))
+        net[f'h2_{i}'].cmd('nohup python3 ping.py h1_{} &'.format(i))
+
+
 
         net['r1'].cmd('ethtool -K r1-eth{} tx off'.format(i+1))
         net['r3'].cmd('ethtool -K r2-eth{} tx off'.format(i+1))
---
+++ ping.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+if __name__ == '__main__':
+    os.system("ping " + sys.argv[1])
